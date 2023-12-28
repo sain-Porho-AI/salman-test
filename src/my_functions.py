ac054efe86a3db20e9388e34ecd400330213c0d9
Salman-18 <salmannizamani205@gmail.com>
2023-12-28 13:22:25 -0800

    new changess

--- src/my_functions.py
+++ src/my_functions.py
@@ -1,6 +1,7 @@
 # Function to add two numbers.
 def add(number_one, number_two):
     return number_one + number_two
+
 # Function to divide two numbers. Raises a ValueError if the divisor is zero.
 def divide(number_one, number_two):
     if number_two == 0:
