03d3c786d895438c90554d75e9fce7d93e7fd211
Salman-18 <salmannizamani205@gmail.com>
2023-12-28 13:32:52 -0800

    change in linting

--- src/shapes.py
+++ src/shapes.py
@@ -1,33 +1,35 @@
 import math
+
+
 class Shape:
     def area(self):
         pass
-           
+
     def perimeter(self):
         pass
-        
+
 
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2 
-    
+        return math.pi * self.radius ** 2
+
     def perimeter(self):
-        return 2  * math.pi * self.radius 
-        
+        return 2  * math.pi * self.radius
+
 
 class Rectangle(Shape):
-    
+
     def __init__(self, length, width):
         self.length = length
         self.width = width
-    
+
     def __eq__(self, other):
         if not isinstance(other , Rectangle):
             return False
-        return self.width == other.width and self.length == other.length 
+        return self.width == other.width and self.length == other.length
 
 
     def area(self):
@@ -35,7 +37,7 @@ class Rectangle(Shape):
     
     def perimeter(self):
         return (self.length * 2) + (self.width * 2)
-    
+
 
 class Square(Rectangle):
     def __init__(self, side_length):
