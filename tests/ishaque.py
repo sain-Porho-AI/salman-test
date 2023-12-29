9cf7e8eaa41bb8708cc200b91a86b996d497f2da
Salman-18 <salmannizamani205@gmail.com>
2023-12-28 16:18:38 -0800

    add new tests

---
+++ tests/ishaque.py
@@ -0,0 +1,5 @@
+import pytest
+import src.shapes as shapes 
+@pytest.mark.parametrize("side_length, expected_area", [(17, 289)])
+def test_multiple_square_areas(side_length, expected_area):
+    assert shapes.Square(side_length).area() == expected_area
