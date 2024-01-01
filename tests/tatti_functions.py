a75fcd6a154813fbf80cedad10a8a0705ac8bd61
Salman-18 <salmannizamani205@gmail.com>
2024-01-01 09:53:45 +0500

    change in only one functuins

--- tests/tatti_functions.py
+++ tests/tatti_functions.py
@@ -1,5 +1,5 @@
 import pytest
 import src.shapes as shapes 
-@pytest.mark.parametrize("side_length, expected_area", [(30, 900), (78, 6084)])
+@pytest.mark.parametrize("side_length, expected_area", [(30, 900), (78, 6084), (12, 144)])
 def test_multiple_square_areas(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
