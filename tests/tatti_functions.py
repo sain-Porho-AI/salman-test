66830f774d7edc85a2b874c13e51cb9ea3f6ac01
Salman-18 <salmannizamani205@gmail.com>
2024-01-01 10:00:22 +0500

    add one more functions11

--- tests/tatti_functions.py
+++ tests/tatti_functions.py
@@ -3,3 +3,6 @@ import src.shapes as shapes
 @pytest.mark.parametrize("side_length, expected_area", [(30, 900), (78, 6084)])
 def test_multiple_square_areas(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
+@pytest.mark.parametrize("side_length, expected_area", [(12, 144)])
+def test_multiple_square_areas(side_length, expected_area):
+    assert shapes.Square(side_length).area() == expected_area
