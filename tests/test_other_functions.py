79333deb74e0bd006c6c5fa720daf8a71fca792a
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-01-01 12:08:50 +0500

    enh: working

--- tests/test_other_functions.py
+++ tests/test_other_functions.py
@@ -1,13 +1,25 @@
 import src.my_functions as my_function
+
+
 def test_for_divide():
     """
     Test function to check the 'divide' function with normal division.
     """
     result = my_function.divide(number_one=100, number_two=10)
     assert result == 10
+
+
 def test_for_divide():
     """
     Test function to check the 'divide' function with normal division.
     """
     result = my_function.divide(number_one=30, number_two=3)
     assert result == 10
+
+
+def test_for_divide():
+    """
+    Test function to check the 'divide' function with normal division.
+    """
+    result = my_function.divide(number_one=30, number_two=2)
+    assert result == 15
