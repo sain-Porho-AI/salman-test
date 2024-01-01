bf71822b17ddf5195bf71cb9a662a541c2f92abb
Salman-18 <salmannizamani205@gmail.com>
2024-01-01 11:20:43 +0500

    work2

--- tests/only_test.py
+++ tests/only_test.py
@@ -61,3 +61,18 @@ def test_add():
     """
     result = my_function.add(number_one=19, number_two=19)
     assert result == 38
+
+def test_divide():
+    """
+    Test function to check the 'divide' function with normal division.
+    """
+    result = my_function.divide(number_one=24, number_two=12)
+    assert result == 2
+
+
+def test_add():
+    """
+    Test function to check the 'add' function with integer inputs.
+    """
+    result = my_function.add(number_one=17, number_two=17)
+    assert result == 34
